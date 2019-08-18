5241d057d2cf64debefa5969e2e6adc4adb21713
michal.lester87@gmail.com <michal.lester87@gmail.com>
2019-08-18 22:37:14 +0200

    add no format with pretier on css files

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,15 @@ import * as styles from './index.module.scss';
 const App = () => (
 	<div className={styles.headerSmall}>
 		<h1>Title</h1>
-		<h2>Subtitle</h2>
-		<p>This is paragraph acctually</p>
+		<div className={styles.wrapper}>
+			<div className={styles.header}>Header</div>
+			<div className={styles.sidebar}>Sidebar</div>
+			<div className={styles.content}>
+				Content
+				<br /> More content than we had before so this column is now quite tall.
+			</div>
+			<div className={styles.footer}>Footer</div>
+		</div>
 	</div>
 );
 
